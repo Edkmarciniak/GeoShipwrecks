f2d7d0ae49cc37a2fff96e4f2a2c348f6d9e9f97
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-01 18:48:41 -0600

    Rename seniors with users

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 import express from "express";
-import seniorRoutes from "./routes/seniorRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/seniors", seniorRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
