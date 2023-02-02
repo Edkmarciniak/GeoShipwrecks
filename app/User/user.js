ac9a93f953c18ff0b327922a2a77a09afb6dcf14
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-02 13:50:37 -0600

    Delete userSchema

--- app/User/user.js
+++ app/User/user.js
@@ -1,21 +1,20 @@
 import { Schema, model } from "mongoose";
-import userSchema from "./user-schema.js";
 
-const seniorSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "User name is required"],
-      minLength: [3, "User name must be at least 3 characters long"],
-      trim: true,
-      validate: {
-        validator(name) {
-          // Only allow letters and spaces (one space in between words)
-          return /[a-zA-Z]+([\s][a-zA-Z]+)*/.test(name);
-        },
-        message:
-          "User name must only contain letters and only one space in between names",
+const seniorSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "User name is required"],
+    minLength: [3, "User name must be at least 3 characters long"],
+    trim: true,
+    validate: {
+      validator(name) {
+        // Only allow letters and spaces (one space in between words)
+        return /[a-zA-Z]+([\s][a-zA-Z]+)*/.test(name);
       },
+      message:
+        "User name must only contain letters and only one space in between names",
     },
+  },
+});
 
 // Start from here tomorrow
