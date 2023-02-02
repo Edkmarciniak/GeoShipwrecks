3e862710ff16aa2ce2719f70f4cbd596d0819b71
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-02 13:30:14 -0600

    Delete: import userRoutes and app.use

--- app/index.js
+++ app/index.js
@@ -1,11 +1,8 @@
 import express from "express";
-import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
