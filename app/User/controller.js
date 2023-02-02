e55b4ba6195c947c391414f9c16c63548b8fc58d
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-01 19:11:28 -0600

    Add imports to controller

---
+++ app/User/controller.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "../config.js";
+import User from "./User.js";
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(config.getDbConn("users"))
+  .then(() => {
+    console.info("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database", err);
+  });
+
+const controller = {
+  getUsers() {
+    return User.find();
+  },
