cf049eb051b0884e11901ec0c0104a3f5f61f497
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-06 15:32:51 -0600

    Create an address schema

---
+++ app/User/address-schema.js
@@ -0,0 +1,26 @@
+const addressSchema = {
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+};
