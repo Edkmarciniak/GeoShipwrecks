a363b60c2bf8058ff6a171214fdc61f7de482578
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-06 16:05:00 -0600

    Create Service.js

---
+++ app/service/Service.js
@@ -0,0 +1,14 @@
+import { Schema } from "mongoose";
+import userSchema from "../User/user-schema";
+
+const serviceSchema = new Schema({
+  name: String,
+  description: String,
+  serviceType: {
+    type: String,
+    enum: ["Food", "Transportation", "Housework", "Other"],
+  },
+  requestedBy: userSchema,
+
+  acceptedBy: userSchema,
+});
