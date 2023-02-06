61579888b893597e1b048cf9e245d5fc6f3b741d
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-06 11:33:32 -0600

    Change getDbConn to dbConn

--- app/config.js
+++ app/config.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  getDbConn(db) {
-    return `${process.env.DB_CONN}/${db}?retryWrites=true&w=majority`;
-  },
+  dbConn: process.env.DB_CONN,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1h",
+  jwtSecret: process.env.JWT_SECRET,
+  port: process.env.PORT || 3000,
+  saltRounds: Number(process.env.SALT_ROUNDS) || 10,
 };
