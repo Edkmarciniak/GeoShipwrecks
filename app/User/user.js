47d8d8f2a934fa987b491721ef5b26bbb8bccf90
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-06 16:02:35 -0600

    Paste from user-schema.js file before deleting file

--- app/User/user.js
+++ app/User/user.js
@@ -23,8 +23,44 @@ const seniorSchema = new Schema({
     trim: true,
   },
   // TODO: Get more stuff from 'user-schema.js' (and then delete that file)
-  services: [serviceSchema],
-});
+  services: [serviceSchema]
+
+  export default new Schema({
+
+    userType: {
+      type: String,
+      enum: ["Senior", "Provider"],
+      default: "Senior",
+    },
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      minLength: [3, "User name must be at least 3 characters long"],
+      trim: true,
+    },
+    email: {
+      type: Number,
+      required: [true, "Email address is required"],
+    },
+    password: {
+      type: Number,
+      required: [true, "Password is required"],
+    },
+    phone: {
+      type: Number,
+      required: [true, "Phone number is required"],
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+    },
+    services: {
+      type: [String],
+      enum: ["Housekeeping", "Laundry", "Meals", "Grooming", "Yardwork","Transportation", "Shopping", "Companionship"],
+    },
+
+  });
+
 
 seniorSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
