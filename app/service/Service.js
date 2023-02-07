f891a24e9394d7b5aaac513aab4729d490b4507d
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-07 08:46:29 -0600

    Create requestedBy and fulfilledBy schemas

--- app/service/Service.js
+++ app/service/Service.js
@@ -6,9 +6,55 @@ const serviceSchema = new Schema({
   description: String,
   serviceType: {
     type: String,
-    enum: ["Food", "Transportation", "Housework", "Other"],
+    enum: [
+      "Housekeeping",
+      "Laundry",
+      "Meals",
+      "Grooming",
+      "Yardwork",
+      "Transportation",
+      "Shopping",
+      "Companionship",
+    ],
   },
-  requestedBy: userSchema,
 
-  fulfilledBy: userSchema,
+  const requestedBySchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+
+  const fulfilledBySchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
 });
