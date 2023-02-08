805bc0a3c582056537f6f2d97a5c165f3224d6cb
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-08 08:56:08 -0600

    npm i ky

--- app/User/routes.js
+++ app/User/routes.js
@@ -18,7 +18,6 @@ router.post("/login", async (req, res) => {
 
   const { username, password } = req.body;
 
-  // Will return a JWT token or null
   const jwt = await userController.login(username, password);
 
   if (jwt) {
