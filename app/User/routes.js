6973e5d7c05116aef1b2a7b8e1b2c2fa3f75a925
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 09:11:53 -0600

    c

--- app/User/routes.js
+++ app/User/routes.js
@@ -4,44 +4,11 @@ import userController from "./controller.js";
 
 const router = new Router();
 
-router.post("/create", async (req, res) => {
-  const { username, password } = req.body;
-
-  userController
-    .create(username, password)
-    .then(() => {
-      userController.login(username, password).catch((err) => {
-        res.status(500).json({ message: err.message });
-      });
-    })
-    .then((jwt) => {
-      res.json({ token: jwt });
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(400).json({ message: err.message });
-      } else if (err.message === "User already exists") {
-        res.status(409).json({ message: err.message });
-      } else {
-        res.status(500).json({ message: err.message });
-      }
-    });
-});
-
-router.post("/login", async (req, res) => {
+router.get("/me", async (req, res) => {
   if (req.user) {
-    return res.json({ message: "You are already logged in" });
-  }
-
-  const { username, password } = req.body;
-
-  // Will return a JWT token or null
-  const jwt = await userController.login(username, password);
-
-  if (jwt) {
-    res.json({ token: jwt });
+    res.json(req.user);
   } else {
-    res.status(401).json({ message: "Invalid credentials" });
+    res.status(401).json({ message: "You are not logged in" });
   }
 });
 
