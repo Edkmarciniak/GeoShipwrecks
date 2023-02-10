8012d2b82d0c8c225fe5a7b1bdfe3a0dcff45e4d
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 10:32:31 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import db from "./db.js";
 
 const app = express();
 
@@ -7,3 +8,6 @@ app.use(express.json());
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
 });
+
+const collections = await db.listCollections().toArray();
+console.log(collections);
