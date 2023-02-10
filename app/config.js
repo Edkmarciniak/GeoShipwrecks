322b40242536f2ed795985191483094b8b909f73
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 09:53:53 -0600

    refactor: ♻️

--- app/config.js
+++ app/config.js
@@ -3,9 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  dbConn: process.env.DB_CONN,
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1h",
-  jwtSecret: process.env.JWT_SECRET,
+  db: {
+    conn: process.env.DB_CONN,
+    name: process.env.DB_NAME,
+  },
+  jwt: {
+    expiresIn: process.env.JWT_EXPIRES_IN || "1h",
+    secret: process.env.JWT_SECRET,
+  },
   port: process.env.PORT || 3000,
   saltRounds: Number(process.env.SALT_ROUNDS) || 10,
 };
