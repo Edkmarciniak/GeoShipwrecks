04f6690d4543d8e0df2395cb2a5356148663939d
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 10:32:00 -0600

    Delete files and update index and db js files
    Co-authored-by: Manav Misra <manavm@visionwebsoft.com>

--- app/db.js
+++ app/db.js
@@ -1,14 +1,21 @@
-import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
 import config from "./config.js";
 
-export default () => {
-  mongoose.set("strictQuery", true);
-  mongoose
-    .connect(config.dbConn)
-    .then(() => {
-      console.info("Connected to the database");
-    })
-    .catch((err) => {
-      console.error("Error connecting to the database", err);
-    });
-};
+const client = new MongoClient(config.db.conn);
+let db;
+
+try {
+  await client.connect();
+  db = client.db(config.db.name);
+} catch (err) {
+  console.error(err.message);
+} finally {
+  await client.close();
+}
+
+process.on("exit", () => {
+  client.close();
+  console.info("MongoDB connection closed");
+});
+
+export default db;
