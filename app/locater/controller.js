4f9ea88b545b3ccdbe1825bc1c670b80bdd91ee7
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 11:03:38 -0600

    feat: :necktie: locator controller

--- app/locater/controller.js
+++ app/locater/controller.js
@@ -1,5 +1,11 @@
 import dbConn from "../db.js";
 
-const controller = {};
+const collection = dbConn.db("shipwrecks").collection("shipfinder");
+
+const controller = {
+  index() {
+    return collection.find().toArray();
+  },
+};
 
 export default controller;
