42cd6c8272f6f09c038f41e9bb26aebdc4a3c4d7
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 08:28:40 -0600

    Replace with cut/paste from grades-express-routes

--- app/User/User.js
+++ app/User/User.js
@@ -3,35 +3,31 @@ import jwt from "jsonwebtoken";
 import { Schema, model } from "mongoose";
 import config from "../config.js";
 
-const userSchema = new Schema({
-  name: {
+const UserSchema = new Schema({
+  username: {
     type: String,
-    required: [true, "Name is required"],
-  },
-  email: {
-    type: Number,
-    required: [true, "Email address is required"],
+    required: [true, "Username is required"],
+    minLength: [3, "Username must be at least 3 characters long"],
+    trim: true,
+    validate: {
+      validator(username) {
+        // Only allow letters and spaces (one space in between words)
+        return /[a-zA-Z]+([\s][a-zA-Z]+)*/.test(username);
+      },
+    },
   },
   password: {
-    type: Number,
-    required: [true, "Password is required"],
-  },
-  phone: {
-    type: Number,
-    required: [true, "Phone number is required"],
-  },
-  address: {
     type: String,
-    required: [true, "Address is required"],
-  },
-  services: {
-    // If 'userType' is 'Senior', then this is a list of services requested
-    // If 'userType' is 'Provider', then this is a list of services provided
-    type: [serviceSchema],
+    required: [true, "Password is required"],
+    minLength: [3, "Password must be at least 3 characters long"],
+    trim: true,
   },
 });
 
-userSchema.pre("save", async function (next) {
+UserSchema.pre("save", async function (next) {
+  this.username = this.username.trim().toLowerCase();
+
+  // * Only hash the password if it has been modified (or is new)
   if (this.isModified("password")) {
     const generatedSalt = await bcrypt.genSalt(config.saltRounds);
     this.password = await bcrypt.hash(this.password, generatedSalt);
@@ -39,11 +35,13 @@ userSchema.pre("save", async function (next) {
 
   next();
 });
-// * If there is a user
-userSchema.statics.login = async function (username, password) {
+
+UserSchema.statics.login = async function (username, password) {
+  // * Find the user by username (case insensitive)
   const user = await this.findOne({ username });
 
   let isMatch = false;
+  // * If there is a user, compare the password
   if (user) {
     isMatch = await bcrypt.compare(password, user.password);
   }
@@ -62,4 +60,4 @@ userSchema.statics.login = async function (username, password) {
     : null;
 };
 
-export default model("User", userSchema);
+export default model("User", UserSchema);
