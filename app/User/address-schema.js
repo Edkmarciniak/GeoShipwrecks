43fa500a33e23782db71956230a2269d84a82c67
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 08:25:37 -0600

    Delete file

--- app/User/address-schema.js
+++
@@ -1,6 +0,0 @@
-const addressSchema = {
-  streetAddress: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  zipcode: { type: Number, required: true },
-};
