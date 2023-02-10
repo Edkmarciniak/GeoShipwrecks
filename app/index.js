b3bdb7d0d345b285814ae19053e889f00444e79f
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 12:19:37 -0600

    Add TODO

--- app/index.js
+++ app/index.js
@@ -1,13 +1,11 @@
 import express from "express";
-import db from "./db.js";
 
 const app = express();
 
 app.use(express.json());
 
+// TODO: Add middleware for all of the user authentication routes
+
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
 });
-
-const collections = await db.listCollections().toArray();
-console.log(collections);
