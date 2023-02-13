b1ff656c1de6e8d0accae5b771ae5bf25052a19f
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-13 09:49:49 -0600

    feat: get shipwrecks from router

--- app/index.js
+++ app/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import shipRouter from "./shipfinder/routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-// TODO: Add middleware for all of the user authentication routes
+app.get("/", (_, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+// http://localhost:3000/shipfinder
+app.use("/shipfinder", shipRouter);
 
 app.listen(3000, () => {
-  console.info("Server is running on port 3000");
+  console.info("Shipwreck Server is running on port 3000");
 });
