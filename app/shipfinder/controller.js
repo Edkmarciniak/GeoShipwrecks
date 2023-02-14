783aec2ad8d9b8105894170762677aa2f324fb5e
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-14 11:53:24 -0600

    Add import config, change shipfinder to shipwrecks, const controller, add TODO

--- app/shipfinder/controller.js
+++ app/shipfinder/controller.js
@@ -1,10 +1,17 @@
 import dbConn from "../db.js";
+import config from "../config.js";
 
-const collection = dbConn.db("shipwrecks").collection("shipfinder");
+const collection = dbConn.db(config.db.name).collection("shipwrecks");
 
 const controller = {
   index() {
-    return collection.find().toArray();
+    return (
+      collection
+        .find()
+        // TODO: Customize this limit number as needed
+        .limit(10)
+        .toArray()
+    );
   },
 };
 
